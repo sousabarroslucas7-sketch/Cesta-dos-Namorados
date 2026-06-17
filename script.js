77c98e6c3c1f914700d56d9ab6ed214c294d780c
esboço sousabarroslucas7 <sousabarroslucas7@gmail.com>
2026-06-17 11:59:40 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function comprarUm (){
+    let mensagem = document.getElementById("mensagem1");
+    mensagem.innerHTML = "Você escolheu a Cesta Premium!";
+}
+
+function comprarDois (){
+    let mensagem = document.getElementById("mensagem2");
+    mensagem.innerHTML = "Você escolheu a Cesta Flores!";
+}
+
+function comprarTres (){
+    let mensagem = document.getElementById("mensagem3");
+    mensagem.innerHTML = "Você escolheu a Cesta Chocolate!";
+}
